6fef6a4fb426f0e3b596ee585d422870ce47af74
wangning <zjuwangning@zju.edu.cn>
2023-02-05 00:33:05 +0800

    index page

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,57 +6,15 @@ Page({
   data: {
     showUploadTip: false,
     powerList: [{
-      title: '云函数',
-      tip: '安全、免鉴权运行业务代码',
-      showItem: false,
-      item: [{
-        title: '获取OpenId',
-        page: 'getOpenId'
-      },
-      //  {
-      //   title: '微信支付'
-      // },
-       {
-        title: '生成小程序码',
-        page: 'getMiniProgramCode'
-      },
-      // {
-      //   title: '发送订阅消息',
-      // }
-    ]
+      title: '武器天赋材料',
+      tip: '武器突破/天赋升级',
+      page: ""
     }, {
-      title: '数据库',
-      tip: '安全稳定的文档型数据库',
-      showItem: false,
-      item: [{
-        title: '创建集合',
-        page: 'createCollection'
-      }, {
-        title: '更新记录',
-        page: 'updateRecord'
-      }, {
-        title: '查询记录',
-        page: 'selectRecord'
-      }, {
-        title: '聚合操作',
-        page: 'sumRecord'
-      }]
+      title: '周本材料',
+      tip: '天赋升级周本材料',
     }, {
-      title: '云存储',
-      tip: '自带CDN加速文件存储',
-      showItem: false,
-      item: [{
-        title: '上传文件',
-        page: 'uploadFile'
-      }]
-    }, {
-      title: '云托管',
-      tip: '不限语言的全托管容器服务',
-      showItem: false,
-      item: [{
-        title: '部署服务',
-        page: 'deployService'
-      }]
+      title: '消耗计算',
+      tip: '突破升级消耗计算器',
     }],
     envList,
     selectedEnv: envList[0],
